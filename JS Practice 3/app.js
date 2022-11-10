5eae45439545c543c148446f91cbbc08d62bea5d
Shahjhansamoo <91805718+Shahjhansamoo@users.noreply.github.com>
2022-11-10 14:17:46 +0500

    initial commit

---
+++ JS Practice 3/app.js
@@ -0,0 +1,85 @@
+// Practice Array POP();
+
+
+document.write("<h1>Array Practice Pop();</h1>");
+
+var a = ["Khurram","Shahjahan","Bilal","Naeem","Sajid"];
+
+document.write(a + "<br><br>");
+
+
+a.pop();
+
+document.write(a + "<br><br>");
+
+
+
+
+// Practice Array Push();
+
+document.write(a + "<h1>Array Practice Push();</h1>");
+
+var a = ["Khurram","Shahjahan","Bilal","Naeem","Sajid"];
+
+document.write(a + "<br><br>");
+
+
+a.push("Basit");
+
+document.write(a + "<br><br>");
+
+
+
+
+
+
+// Practice Array Slice();
+
+document.write(a + "<h1>Array Practice Slice();</h1>");
+
+var a = ["Khurram","Shahjahan","Bilal","Naeem","Sajid"];
+
+document.write(a + "<br><br>");
+
+
+var b = a.slice(1,4);
+
+document.write(b + "<br><br>");
+
+
+
+
+
+
+
+
+
+// Practice Array Splice();
+
+document.write(a + "<h1>Array Practice Splice();</h1>");
+
+var a = ["Khurram","Shahjahan","Bilal","Naeem","Sajid"];
+
+document.write(a + "<br><br>");
+
+a.splice(2,0,"Zunair","Mughal")
+
+
+document.write(a + "<br><br>");
+
+
+
+
+
+
+
+// Practice For Loop();
+document.write(a + "<h1>Practice For Loop();</h1>");
+
+
+
+for (var a = 1; a <= 10; a++){
+
+    document.write("jawan  Pakistan<br>");
+}
+
